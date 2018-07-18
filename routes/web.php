a1a3360995711f4c9548b3e57666a2141568a2eb
Jayzen <578393193@qq.com>
2018-07-19 00:06:03 +0800

    crud

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/shop_categories','Shop_categoriesController');
+Route::resource('/shops','ShopsController');
+
+//Route::get('/user/{user}', 'UsersController@show')->name('users.show');//查看单个用户信息
+//Route::get('/users/status','UsersController@status')->name('users.status');
+Route::get('/users_status/{user}', 'UsersController@status')->name('users.status');
+Route::get('/shops_status/{shop}', 'ShopsController@status')->name('shops.status');
+Route::resource('/users','UsersController');
